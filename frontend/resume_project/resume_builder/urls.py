a3ac3f496a3f48c7c7106fd33eeac435b22f7e7f
Ariane Souza <156832391+Arireal@users.noreply.github.com>
2026-01-08 16:41:43 -0300

    Delete frontend/resume_project directory

--- frontend/resume_project/resume_builder/urls.py
+++
@@ -1,8 +0,0 @@
-# resume_builder/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.resume_create, name='index'),
-]
-
--- frontend/resume_project/resume_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('resume/', include('resume_builder.urls')),  # This is fine as it points to your resume_builder app
-    path('', include('resume_builder.urls')),  # Add this line to link the root path to your app's URLs
-]
